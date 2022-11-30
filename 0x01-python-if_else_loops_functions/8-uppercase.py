19a2637ab4a7d68ce8a0e3450e243ee194653337
DaffiX <suleydaffy@gmail.com>
2022-11-29 18:36:36 -0800

    print last digit

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+
+            print("{:d}{:d}".format(num, num2), end=", ")
+            princse(str):
+    for i in str:
+        i = ord(i)
+        if 97 <= i <= 122:
+            i = i - 32
+        print("{:c}".format(i), end='')
+    print()("{:d}{:d}".format(num, num2), end=", ")
