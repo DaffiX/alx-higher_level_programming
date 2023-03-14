e43fbde2f6489785257e3f056bcfe8fd7ed24d16
DaffiX <suleydaffy@gmail.com>
2023-03-14 02:57:25 +0000

    Passing Error FIxed

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,12 +6,4 @@ if (isNaN(sqr)) {
   for (let x = 0; x < sqr; x++) {
     console.log('X'.repeat(sqr));
   }
-}#!/usr/bin/node
-const sqr = process.argv[2];
-if (isNaN(sqr)) {
-  console.log('Missing size');
-} else {
-  for (let x = 0; x < sqr; x++) {
-    console.log('X'.repeat(sqr));
-  }
-}
+}}
