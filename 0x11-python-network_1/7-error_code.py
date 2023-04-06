b6d4e05918a56da92af7447305f1b5f263acfeba
DaffiX <suleydaffy@gamil.com>
2023-04-05 19:26:24 -0700

    Search API with POST Req

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+!/usr/bin/python3
+"""Sends a Request then.
+Python script that takes in a URL,
+sends a request to the URL and displays the body of the response.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    meg = requests.get(url)
+    if meg.status_code >= 400:
+        print("Error code: {}".format(meg.status_code))
+    else:
+        print(meg.text)
